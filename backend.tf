031f6ae12d3ed3807284eec25fd408f5a5c595ea
ash-ley <ashleyritchie1871@hotmail.co.uk>
2021-12-10 10:26:42 +0000

    REMOVE - state lock

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "talent-academy-439272626435-tfstate-ashley"
     key            = "sprint3/ansible-ec2-demo/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
+    #dynamodb_table = "terraform-lock"
   }
 }
