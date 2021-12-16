4a97c695b6c6363040565115373388cbf2b639bd
ash-ley <ashleyritchie1871@hotmail.co.uk>
2021-12-16 17:34:19 +0000

    UPDATE - opened ingress port 80

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "my_app_sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["81.108.153.97/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
